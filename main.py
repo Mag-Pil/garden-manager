18eed3bd2b33d5ad50bf987cd5bc6b8b26082d1e
Magdalena Piłakowska <mpilakowska@gmail.com>
2026-07-21 18:05:43 +0200

    Translate garden data model to English

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ class Plant:
         self.variety = variety
         self.category = category
 
+
 def print_plant(plant):
-    print(f"Nazwa rośliny: {plant.plant_name}, odmiana: {plant.variety}, kategoria: {plant.category}")
+    print(f"Plant name: {plant.plant_name}, variety: {plant.variety}, category: {plant.category}")
 
 class GardenBed:
     def __init__(self, bed_name, list_of_plants=None):
@@ -22,12 +23,13 @@ class GardenBed:
 
         self.number_of_plants = number_of_plants
 
+
 def print_garden_bed(garden_bed):
-    print(f"Grządka: {garden_bed.bed_name}")
-    print(f"Lista roślin na grządce:")
+    print(f"Garden bed: {garden_bed.bed_name}")
+    print(f"List of plants:")
     for plant in garden_bed.list_of_plants:
         print(f"- {plant.plant_name} {plant.variety}")
-    print(f"Liczba roślin na grządce: {garden_bed.number_of_plants}")
+    print(f"Number of plants: {garden_bed.number_of_plants}")
 
 class Tree:
     def __init__(self, species, variety, approximate_age):
@@ -35,8 +37,9 @@ class Tree:
         self.variety = variety
         self.approximate_age = int(approximate_age)
 
+
 def print_tree(tree):
-    print(f"Gatunek drzewa: {tree.species}, odmiana: {tree.variety}, przybliżony wiek: {tree.approximate_age} lat")
+    print(f"Species: {tree.species}, variety: {tree.variety}, approximate age: {tree.approximate_age} years")
 
 class GardenTask:
     def __init__(self, task_name, garden_part, execution_status):
@@ -44,21 +47,22 @@ class GardenTask:
         self.garden_part = garden_part
         self.execution_status = execution_status
 
+
 def print_garden_task(garden_task):
-    print(f"Nazwa zadania: {garden_task.task_name}")
-    print(f"Część ogrodu: {garden_task.garden_part}")
-    print(f"Status wykonania: {garden_task.execution_status}")
+    print(f"Task name: {garden_task.task_name}")
+    print(f"Garden part: {garden_task.garden_part}")
+    print(f"Execution status: {garden_task.execution_status}")
 
 
 def run_example():
-    calendula = Plant(plant_name="Nagietek", variety="lekarski", category="Kwiaty")
-    raspberry_tomato = Plant(plant_name="Pomidor", variety="malinowy", category="Warzywa")
-    san_marzano_tomato = Plant(plant_name="Pomidor", variety="San Marzano", category="Warzywa")
-    black_hungarian = Plant(plant_name="Papryka", variety="czarna węgierska", category="Warzywa")
-    ethiopian_brown = Plant(plant_name="Papryka", variety="etiopska", category="Warzywa")
-    beet = Plant(plant_name="Burak", variety="ćwikłowy", category="Warzywa")
-    carrot = Plant(plant_name="Marchewka", variety="Berlikumer", category="Warzywa")
-    cosmos = Plant(plant_name="Kosmos", variety="pierzasty", category="Kwiaty")
+    calendula = Plant(plant_name="Calendula", variety="officinalis", category="Flowers")
+    raspberry_tomato = Plant(plant_name="Tomato", variety="Raspberry", category="Vegetables")
+    san_marzano_tomato = Plant(plant_name="Tomato", variety="San Marzano", category="Vegetables")
+    black_hungarian = Plant(plant_name="Pepper", variety="Black Hungarian", category="Vegetables")
+    ethiopian_brown = Plant(plant_name="Pepper", variety="Ethiopian Brown", category="Vegetables")
+    beet = Plant(plant_name="Beet", variety="red", category="Vegetables")
+    carrot = Plant(plant_name="Carrot", variety="Berlikumer", category="Vegetables")
+    cosmos = Plant(plant_name="Cosmos", variety="bipinnatus", category="Flowers")
     print_plant(calendula)
     print_plant(raspberry_tomato)
     print_plant(san_marzano_tomato)
@@ -69,21 +73,21 @@ def run_example():
     print_plant(cosmos)
     print(10 * "-")
 
-    vegetable_bed_1 = GardenBed(bed_name="Grządka warzywna przy winogronach", list_of_plants=[calendula,raspberry_tomato,san_marzano_tomato,black_hungarian, ethiopian_brown, beet, carrot, cosmos])
+    vegetable_bed_1 = GardenBed(bed_name="Vegetable bed next to the grapevine", list_of_plants=[calendula,raspberry_tomato,san_marzano_tomato,black_hungarian, ethiopian_brown, beet, carrot, cosmos])
     print_garden_bed(vegetable_bed_1)
     print(10 * "-")
 
-    quince_tree_1 = Tree(species="Pigwa", variety="gruszkowa", approximate_age=10)
+    quince_tree_1 = Tree(species="Quince", variety="oblonga", approximate_age=10)
     print_tree(quince_tree_1)
-    quince_tree_2 = Tree(species="Pigwa", variety="gruszkowa", approximate_age=10)
+    quince_tree_2 = Tree(species="Quince", variety="oblonga", approximate_age=10)
     print_tree(quince_tree_2)
-    grafted_chokeberry = Tree(species="Aronia", variety="czarna, szczepiona na jarzębinie", approximate_age=10)
+    grafted_chokeberry = Tree(species="Chokeberry", variety="black", approximate_age=10)
     print_tree(grafted_chokeberry)
-    plum_tree = Tree(species="Śliwa", variety="nieznana", approximate_age=10)
+    plum_tree = Tree(species="Plum", variety="unknown", approximate_age=10)
     print_tree(plum_tree)
     print(10 * "-")
 
-    tree_pruning_1 = GardenTask(task_name="Przycinanie drzew", garden_part="mniejszy sad", execution_status="odłożone do zimy")
+    tree_pruning_1 = GardenTask(task_name="Tree pruning", garden_part="smaller orchard", execution_status="postponed until winter")
     print_garden_task(tree_pruning_1)
 
 if __name__ == '__main__':
